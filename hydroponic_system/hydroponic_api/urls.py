f94d867fb9bf9e758fcd0bced68c804863650ac5
Cheburator <sashadanilenko06@gmail.com>
2025-02-02 23:45:28 +0100

    list of all measurements removed from root

--- hydroponic_system/hydroponic_api/urls.py
+++ hydroponic_system/hydroponic_api/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path, include
+from django.urls import path, include, reverse
 from rest_framework.routers import DefaultRouter
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .views import HydroponicSystemViewSet, MeasurementViewSet
 
 router = DefaultRouter()
 router.register('systems', HydroponicSystemViewSet, basename='hydroponicsystem')
 router.register('measurements', MeasurementViewSet, basename='measurement')
 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'systems': request.build_absolute_uri(reverse('hydroponicsystem-list'))
+    })
+
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('', include(router.urls)),
 ]
