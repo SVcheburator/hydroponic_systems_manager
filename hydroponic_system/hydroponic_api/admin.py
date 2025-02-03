bc0d2426fff976f97a58294465b9230513857eeb
Cheburator <sashadanilenko06@gmail.com>
2025-02-04 00:17:06 +0100

    admin panel for existing models created

--- hydroponic_system/hydroponic_api/admin.py
+++ hydroponic_system/hydroponic_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Measurement, HydroponicSystem
 
-# Register your models here.
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'name', 'description', 'created_at')
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'system', 'ph', 'temperature', 'tds', 'time')
+
+
+admin.site.register(HydroponicSystem, HydroponicSystemAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
--- hydroponic_system/hydroponic_api/models.py
+++ hydroponic_system/hydroponic_api/models.py
@@ -3,21 +3,29 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class HydroponicSystem(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="systems")
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, verbose_name="Owner", on_delete=models.CASCADE, related_name="systems")
+    name = models.CharField("Name", max_length=100)
+    description = models.TextField("Description", blank=True, null=True)
+    created_at = models.DateTimeField("Created_at", auto_now_add=True)
 
     def __str__(self):
         return self.name
     
+    class Meta:
+        verbose_name = "HydroponicSystem"
+        verbose_name_plural = "HydroponicSystems"
+
 
 class Measurement(models.Model):
-    system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE, related_name="measurements")
-    ph = models.FloatField()
-    temperature = models.FloatField()
-    tds = models.FloatField()
-    time = models.DateTimeField(auto_now_add=True)
+    system = models.ForeignKey(HydroponicSystem, verbose_name="System", on_delete=models.CASCADE, related_name="measurements")
+    ph = models.FloatField("pH")
+    temperature = models.FloatField("temperature")
+    tds = models.FloatField("tds")
+    time = models.DateTimeField("Time", auto_now_add=True)
 
     def __str__(self):
         return f"Measurement of {self.system.name} system at {self.time}"
+    
+    class Meta:
+        verbose_name = "Measurement"
+        verbose_name_plural = "Measurements"
